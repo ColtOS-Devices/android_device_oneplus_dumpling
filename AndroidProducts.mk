eec4ad93117b1cc4b80cef34fd06215dd548c47a
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    dumpling: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The Xtended Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/colt_dumpling.mk
+
+COMMON_LUNCH_CHOICES := \
+    colt_dumpling-user \
+    colt_dumpling-userdebug \
+    colt_dumpling-eng
